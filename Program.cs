dd51046af5d3e6de94775254140a3a3d4a39ae1e
Laursen1992 <47735401+Laursen1992@users.noreply.github.com>
2019-02-18 11:10:00 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TFS_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        { 
+            Console.WriteLine("hej");
+
+            Console.ReadLine();
+        }
+    }
+}
